c114e97a5f50b99245dfa1658b47093538a08179
advansoftware <brunoantunes94@hotmail.com>
2025-03-14 03:35:13 -0300

    ajsutando pre build

--- app.config.ts
+++ app.config.ts
@@ -1,20 +1,20 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 const config = {
-  name: 'GastoMetria',
-  version: '1.0.0',
+  name: "GastoMetria",
+  version: "1.0.0",
   android: {
-    package: "com.brunoantunes100.gastometria"
+    package: "com.brunoantunes100.gastometria",
+  },
+  ios: {
+    bundleIdentifier: "com.brunoantunes100.gastometria",
   },
   extra: {
     openaiApiKey: process.env.OPENAI_API_KEY,
     nodeEnv: process.env.NODE_ENV,
   },
-  plugins: [
-    'expo-router',
-    'expo-barcode-scanner'
-  ],
-  scheme: 'gastometria'
+  plugins: ["expo-router", "expo-barcode-scanner"],
+  scheme: "gastometria",
 };
 
 export default config;
